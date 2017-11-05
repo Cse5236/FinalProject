c91ff8a6543cdf137de0408f2f36ae0d178c3fb7
Starfwy <starfwy@hotmail.com>
2017-11-05 17:43:25 -0500

    Add to register when click on login with wrong email/password.

--- app/src/main/java/com/example/whath/ui/LoginActivity.java
+++ app/src/main/java/com/example/whath/ui/LoginActivity.java
@@ -4,9 +4,11 @@ package com.example.whath.ui;
  * Created by Star on 2017/11/5.
  */
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -15,6 +17,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.whath.ui.accelerometer.bump;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -57,22 +62,63 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void login(View v){
-        String email = ((EditText)findViewById(R.id.email))
+        final String email = ((EditText)findViewById(R.id.email))
                 .getText().toString();
-        String password = ((EditText)findViewById(R.id.password))
+        final String password = ((EditText)findViewById(R.id.password))
                 .getText().toString();
         Log.d("authentication", email+"/"+password);
-        auth.signInWithEmailAndPassword(email, password);
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Button buttonlogin = (Button) findViewById(R.id.button_flogin);
+                            buttonlogin.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Toast.makeText(LoginActivity.this, "Already login", Toast.LENGTH_SHORT).show();
+                                    Intent intent3 = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent3);
+                                }
 
-        Button buttonlogin = (Button) findViewById(R.id.button_flogin);
-        buttonlogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(LoginActivity.this, "Already login", Toast.LENGTH_SHORT).show();
-                Intent intent3 = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent3);
-            }
+                            });
+                        }
+                        else{
+                            Log.d("onComplete", "Login unsuccessful");
+                            register(email, password);
+                        }
+                    }
+                });
+    }
 
-        });
+    private void register(final String email, final String password) {
+        new AlertDialog.Builder(LoginActivity.this)
+                .setTitle("Alert")
+                .setMessage("No existing account. Go to register?")
+                .setPositiveButton("Register",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                createUser(email, password);
+                            }
+                        })
+                .setNeutralButton("Login cancel", null)
+                .show();
+    }
+
+    private void createUser(String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                String message =
+                                        task.isSuccessful() ? "Success" : "Failed";
+                                new AlertDialog.Builder(LoginActivity.this)
+                                        .setMessage(message)
+                                        .setPositiveButton("OK", null)
+                                        .show();
+                            }
+                        });
     }
 }
